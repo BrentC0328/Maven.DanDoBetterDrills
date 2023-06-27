4de98c4680636f85590c4bdcfd588c0b61555802
Brent <brent@Zipcoders-MacBook-Pro-3.local>
2023-06-27 17:41:02 -0400

    completed, full test passing

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.danny_do_better_exercises;
 
+import java.util.Arrays;
+
 /**
  * Created by dan on 6/14/17.
  */
@@ -7,8 +9,16 @@ public class StringUtilities {
     /**
      * @return `Hello World` as a string
      */
+
+    public static Boolean isEven(int x){
+        if(x % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+
     public static String getHelloWorld() {
-        return null;
+        return "Hello World";
     }
 
     /**
@@ -17,7 +27,7 @@ public class StringUtilities {
      * @return the concatenation of two strings, `firstSegment`, and `secondSegment`
      */
     public static String concatenation(String firstSegment, String secondSegment){
-        return null;
+        return firstSegment + secondSegment;
     }
 
     /**
@@ -26,7 +36,7 @@ public class StringUtilities {
      * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
      */
     public static String concatenation(int firstSegment, String secondSegment){
-        return null;
+        return (firstSegment + "") + secondSegment;
     }
 
     /**
@@ -34,7 +44,8 @@ public class StringUtilities {
      * @return the first 3 characters of `input`
      */
     public static String getPrefix(String input){
-        return null;
+
+        return input.substring(0,3);
     }
 
     /**
@@ -42,7 +53,7 @@ public class StringUtilities {
      * @return the last 3 characters of `input`
      */
     public static String getSuffix(String input){
-        return null;
+        return input.substring(input.length() - 3);
     }
 
     /**
@@ -51,15 +62,30 @@ public class StringUtilities {
      * @return the equivalence of two strings, `inputValue` and `comparableValue`
      */
     public static Boolean compareTwoStrings(String inputValue, String comparableValue){
-        return null;
+        if (inputValue == comparableValue){
+            return true;
+        }
+        return false;
     }
 
     /**
      * @param inputValue the value input from user
      * @return the middle character of `inputValue`
      */
+
+
     public static Character getMiddleCharacter(String inputValue){
-        return null;
+        int inputSize = inputValue.length();
+        char[] inputArray = inputValue.toCharArray();
+        char result ;
+
+      if (isEven(inputSize)){
+          result = inputArray[(inputSize / 2) - 1];
+        }
+        else {
+            result = inputArray[(inputSize / 2) ];
+        }
+        return result;
     }
 
     /**
@@ -67,7 +93,8 @@ public class StringUtilities {
      * @return the first sequence of characters
      */
     public static String getFirstWord(String spaceDelimitedString){
-        return null;
+        String result = spaceDelimitedString.split(" ")[0];
+        return result;
     }
 
     /**
@@ -75,7 +102,8 @@ public class StringUtilities {
      * @return the second word of a string delimited by spaces.
      */
     public static String getSecondWord(String spaceDelimitedString){
-        return null;
+        String result = spaceDelimitedString.split(" ")[1];
+        return result;
     }
 
     /**
@@ -83,6 +111,15 @@ public class StringUtilities {
      * @return an identical string with characters in reverse order.
      */
     public static String reverse(String stringToReverse){
-        return null;
+        char[] inputString = stringToReverse.toCharArray();
+        char[] reversedStringArray = new char[inputString.length];
+        int stringLength = inputString.length;
+
+        for (int i = 0; i < stringLength; i++){
+            reversedStringArray[i] = inputString[stringLength - (i + 1)];
+        }
+        String result = String.valueOf(reversedStringArray);
+
+        return result;
     }
 }
